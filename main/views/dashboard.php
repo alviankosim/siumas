52f57b0c98d77a24599300ee2ffc96d4e1580990
alviankosim <alviankosim14@gmail.com>
2023-03-19 14:16:52 +0700

    update dashboard

--- main/views/dashboard.php
+++ main/views/dashboard.php
@@ -225,12 +225,12 @@
                         <div class="card mb-4 text-white bg-warning">
                             <div class="card-body pb-0 d-flex justify-content-between align-items-start">
                                 <div>
-                                    <div class="fs-4 fw-semibold">95% 
+                                    <div class="fs-4 fw-semibold">95%
                                         <!-- <span class="fs-6 fw-normal">(84.7%
                                             <svg class="icon">
                                                 <use xlink:href="<?= base_url('', true) ?>vendors/@coreui/icons/svg/free.svg#cil-arrow-top"></use>
                                             </svg>)</span> -->
-                                        </div>
+                                    </div>
                                     <div>Dana Iuran Terserap</div>
                                 </div>
                                 <div class="dropdown">
@@ -343,6 +343,172 @@
                     </div> -->
                 </div>
                 <!-- /.card.mb-4-->
+                <!-- /.row-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card mb-4">
+                            <div class="card-header">Ringkasan Informasi dan Detil Pengeluaran</div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <div class="border-start border-start-4 border-start-info px-3 mb-3"><small class="text-medium-emphasis">Pengeluaran Baru Bulan Ini</small>
+                                                    <div class="fs-5 fw-semibold">2</div>
+                                                </div>
+                                            </div>
+                                            <!-- /.col-->
+                                            <div class="col-6">
+                                                <div class="border-start border-start-4 border-start-danger px-3 mb-3"><small class="text-medium-emphasis">Pengeluaran on Progress</small>
+                                                    <div class="fs-5 fw-semibold">5</div>
+                                                </div>
+                                            </div>
+                                            <!-- /.col-->
+                                        </div>
+                                        <!-- /.row-->
+                                        <hr class="mt-0">
+                                    </div>
+                                    <!-- /.col-->
+                                    <div class="col-sm-6">
+                                        <div class="row">
+                                            <!-- /.col-->
+                                            <div class="col-6">
+                                                <div class="border-start border-start-4 border-start-success px-3 mb-3"><small class="text-medium-emphasis">Pengeluaran Terealisasi</small>
+                                                    <div class="fs-5 fw-semibold">178</div>
+                                                </div>
+                                            </div>
+                                            <!-- /.col-->
+                                            <div class="col-6">
+                                                <div class="border-start border-start-4 border-start-warning px-3 mb-3"><small class="text-medium-emphasis">Total Pengeluaran</small>
+                                                    <div class="fs-5 fw-semibold">Rp 128.020.000</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- /.row-->
+                                        <hr class="mt-0">
+                                    </div>
+                                    <!-- /.col-->
+                                </div>
+                                <!-- /.row--><br>
+                                <div class="table-responsive">
+                                    <table class="table border mb-0">
+                                        <thead class="table-light fw-semibold">
+                                            <tr class="align-middle">
+                                                <th class="text-center">
+                                                    <svg class="icon">
+                                                        <use xlink:href="<?= base_url('', true) ?>vendors/@coreui/icons/svg/free.svg#cil-image"></use>
+                                                    </svg>
+                                                </th>
+                                                <th>Pengeluaran</th>
+                                                <th class="text-right">Total</th>
+                                                <th>Progress</th>
+                                                <th class="text-center">Tanggal</th>
+                                                <th>Jenis</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr class="align-middle">
+                                                <td class="text-center" style="vertical-align:middle;">
+                                                    <div style="width: 150px;height: 150px;display: block;margin-left: auto;margin-right: auto;background:url('https://sipil.ft.undip.ac.id/wp-content/uploads/2021/09/Picture1-1.jpg');background-position:center;background-size:cover">
+                                                        <!-- <img src="https://sipil.ft.undip.ac.id/wp-content/uploads/2021/09/Picture1-1.jpg" class="rounded img-fluid"> -->
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div>Yiorgos Avraamu</div>
+                                                    <div class="small text-medium-emphasis"><span>New</span> | Registered: Jan 1, 2020</div>
+                                                </td>
+                                                <td class="text-center">
+                                                    <svg class="icon icon-xl">
+                                                        <use xlink:href="<?= base_url('', true) ?>vendors/@coreui/icons/svg/flag.svg#cif-us"></use>
+                                                    </svg>
+                                                </td>
+                                                <td>
+                                                    <div class="clearfix">
+                                                        <div class="float-start">
+                                                            <div class="fw-semibold">50%</div>
+                                                        </div>
+                                                        <div class="float-end"><small class="text-medium-emphasis">Jun 11, 2020 - Jul 10, 2020</small></div>
+                                                    </div>
+                                                    <div class="progress progress-thin">
+                                                        <div class="progress-bar bg-success" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                </td>
+                                                <td class="text-center">
+                                                    <svg class="icon icon-xl">
+                                                        <use xlink:href="<?= base_url('', true) ?>vendors/@coreui/icons/svg/brand.svg#cib-cc-mastercard"></use>
+                                                    </svg>
+                                                </td>
+                                                <td>
+                                                    <div class="small text-medium-emphasis">Last login</div>
+                                                    <div class="fw-semibold">10 sec ago</div>
+                                                </td>
+                                                <td>
+                                                    <div class="dropdown">
+                                                        <button class="btn btn-transparent p-0" type="button" data-coreui-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                            <svg class="icon">
+                                                                <use xlink:href="<?= base_url('', true) ?>vendors/@coreui/icons/svg/free.svg#cil-options"></use>
+                                                            </svg>
+                                                        </button>
+                                                        <div class="dropdown-menu dropdown-menu-end"><a class="dropdown-item" href="#">Info</a><a class="dropdown-item" href="#">Edit</a><a class="dropdown-item text-danger" href="#">Delete</a></div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <tr class="align-middle">
+                                                <td class="text-center" style="vertical-align:middle;">
+                                                    <div style="width: 150px;height: 150px;display: block;margin-left: auto;margin-right: auto;background:url('https://i0.wp.com/www.emporioarchitect.com/upload/portofolio/jasa-arsitek-bogor-tajur-jawa-barat-desain-mushola-green-bamboo-terrace-114449314221020100523-0.jpg');background-position:center;background-size:cover">
+                                                        <!-- <img src="https://i0.wp.com/www.emporioarchitect.com/upload/portofolio/jasa-arsitek-bogor-tajur-jawa-barat-desain-mushola-green-bamboo-terrace-114449314221020100523-0.jpg" class="rounded img-fluid"> -->
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div>Yiorgos Avraamu</div>
+                                                    <div class="small text-medium-emphasis"><span>New</span> | Registered: Jan 1, 2020</div>
+                                                </td>
+                                                <td class="text-center">
+                                                    <svg class="icon icon-xl">
+                                                        <use xlink:href="<?= base_url('', true) ?>vendors/@coreui/icons/svg/flag.svg#cif-us"></use>
+                                                    </svg>
+                                                </td>
+                                                <td>
+                                                    <div class="clearfix">
+                                                        <div class="float-start">
+                                                            <div class="fw-semibold">50%</div>
+                                                        </div>
+                                                        <div class="float-end"><small class="text-medium-emphasis">Jun 11, 2020 - Jul 10, 2020</small></div>
+                                                    </div>
+                                                    <div class="progress progress-thin">
+                                                        <div class="progress-bar bg-success" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                </td>
+                                                <td class="text-center">
+                                                    <svg class="icon icon-xl">
+                                                        <use xlink:href="<?= base_url('', true) ?>vendors/@coreui/icons/svg/brand.svg#cib-cc-mastercard"></use>
+                                                    </svg>
+                                                </td>
+                                                <td>
+                                                    <div class="small text-medium-emphasis">Last login</div>
+                                                    <div class="fw-semibold">10 sec ago</div>
+                                                </td>
+                                                <td>
+                                                    <div class="dropdown">
+                                                        <button class="btn btn-transparent p-0" type="button" data-coreui-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                            <svg class="icon">
+                                                                <use xlink:href="<?= base_url('', true) ?>vendors/@coreui/icons/svg/free.svg#cil-options"></use>
+                                                            </svg>
+                                                        </button>
+                                                        <div class="dropdown-menu dropdown-menu-end"><a class="dropdown-item" href="#">Info</a><a class="dropdown-item" href="#">Edit</a><a class="dropdown-item text-danger" href="#">Delete</a></div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.col-->
+                </div>
+                <!-- /.row-->
             </div>
         </div>
         <footer class="footer">
